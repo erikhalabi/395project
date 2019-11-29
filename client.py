8bde8adfbd35a2082bba3f3b48243d20507a92f7
Bhavish Bhunjun <48217282+bhunjun@users.noreply.github.com>
2019-11-29 15:33:51 -0700

    add non working large file transfer

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
-#-------------------------------------------------------------
+# -------------------------------------------------------------
 # Names: Bhavish Bhunjun, Erik Halabi, Dhaneshen Moonain
 # Program: client.py
-#-------------------------------------------------------------
+# -------------------------------------------------------------
 
 import socket
 import sys
@@ -12,42 +12,43 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 
+
 # This function takes a string as parameter.
 # and returns as encrypted version of the string.
 def encryAES(message, key):
+    # Generate Cyphering Block
+    cipher = AES.new(key, AES.MODE_ECB)
 
-        # Generate Cyphering Block
-        cipher = AES.new(key, AES.MODE_ECB)
+    # Encrypt the message
+    ct_bytes = cipher.encrypt(pad(message.encode('ascii'), 16))
 
-        # Encrypt the message
-        ct_bytes = cipher.encrypt(pad(message.encode('ascii'), 16))
+    return ct_bytes
 
-        return ct_bytes
 
 # Description: This will encrypt the file since the file is already encoded in bytes
 # Parameters: the file contents and the key
 # Return: The encypted file contents 
 def encryptFile(data, key):
+    # Generate Cyphering Block
+    cipher = AES.new(key, AES.MODE_ECB)
 
-        # Generate Cyphering Block
-        cipher = AES.new(key, AES.MODE_ECB)
+    # Encrypt the message
+    ct_bytes = cipher.encrypt(pad(data, 16))
 
-        # Encrypt the message
-        ct_bytes = cipher.encrypt(pad(data, 16))
-
-        return ct_bytes
+    return ct_bytes
 
 
 def decryAES(message, key):
-        # Generate ciphering
-        cipher = AES.new(key, AES.MODE_ECB)
+    # Generate ciphering
+    cipher = AES.new(key, AES.MODE_ECB)
 
-        #Decrypt message
-        Padded_message = cipher.decrypt(message)
-        # Remove padding
-        decryMessage = unpad(Padded_message, 16)
+    # Decrypt message
+    Padded_message = cipher.decrypt(message)
+    # Remove padding
+    decryMessage = unpad(Padded_message, 16)
 
-        return decryMessage.decode('ascii')
+    return decryMessage.decode('ascii')
+
 
 # This is the RSA encrypting function, using the public key
 # It takes 2 parameters: The message to be encrypted
@@ -55,15 +56,16 @@ def decryAES(message, key):
 # required .pem file.
 # It returns the encoded message.
 def encryRSA(message, clientName):
-        filename = clientName + "_public.pem"
-        f = open(filename, "rb")
-        public_key = f.read()
-        pubkey = RSA.import_key(public_key)
-        cipher_rsa_en = PKCS1_OAEP.new(pubkey)
-        enc_data = cipher_rsa_en.encrypt(message.encode('ascii'))
-        f.close()
+    filename = clientName + "_public.pem"
+    f = open(filename, "rb")
+    public_key = f.read()
+    pubkey = RSA.import_key(public_key)
+    cipher_rsa_en = PKCS1_OAEP.new(pubkey)
+    enc_data = cipher_rsa_en.encrypt(message.encode('ascii'))
+    f.close()
 
-        return enc_data
+    return enc_data
+
 
 # This is the RSA decrypting function, using private key.
 # It takes 2 parameters: The message to be decrypted
@@ -71,131 +73,133 @@ def encryRSA(message, clientName):
 # required .pem file.
 # It returns the decrypted message.
 def decryRSA(message, clientName):
-        filename = clientName + "_private.pem"
-        f = open(filename, "rb")
-        private_key = f.read()
-        priv_key = RSA.import_key(private_key)
-        cipher_rsa_dec = PKCS1_OAEP.new(priv_key)
-        dec_data = cipher_rsa_dec.decrypt(message)
-        f.close()
-        #return dec_data.decode('ascii')
-        return dec_data
+    filename = clientName + "_private.pem"
+    f = open(filename, "rb")
+    private_key = f.read()
+    priv_key = RSA.import_key(private_key)
+    cipher_rsa_dec = PKCS1_OAEP.new(priv_key)
+    dec_data = cipher_rsa_dec.decrypt(message)
+    f.close()
+    # return dec_data.decode('ascii')
+    return dec_data
 
-# Description: It will get a file, read it in binary, then close it 
+
+# Description: It will get a file, read it in binary, then close it
 # Parameters: File name to get as string
 # Return: The file contents 
 def getFile(fileName):
-        f = open(fileName, "rb")
-        data = f.read()
-        f.close()
-        return data
+    f = open(fileName, "rb")
+    data = f.read()
+    f.close()
+    return data
+
 
 def client():
+    # Ask for the nerver to connnect to
+    name = input("Enter the server IP or name: ")
 
-        # Ask for the nerver to connnect to
-        name = input("Enter the server IP or name: ")
+    # Server Information
+    serverName = name
+    serverPort = 13000
 
-        # Server Information
-        serverName = name
-        serverPort = 13000
+    # Create client socket that useing IPv4 and TCP protocols
+    try:
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print('Error in client socket creation:', e)
+        sys.exit(1)
 
-        # Create client socket that useing IPv4 and TCP protocols
-        try:
-                clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error as e:
-                print('Error in client socket creation:',e)
-                sys.exit(1)
+    try:
+        # Client connect with the server
+        clientSocket.connect((serverName, serverPort))
 
-        try:
-                # Client connect with the server
-                clientSocket.connect((serverName,serverPort))
+        # Client receives welcome message and prompted to input name.
+        # welcomeMessage = clientSocket.recv(2048)
+        # print(welcomeMessage.decode('ascii'))
 
-                # Client receives welcome message and prompted to input name.
-                #welcomeMessage = clientSocket.recv(2048)
-                #print(welcomeMessage.decode('ascii'))
+        # Client sends name to server
+        clientName = input("Enter client name: ")
+        clientNameEncry = encryRSA(clientName, "server")
+        clientSocket.send(clientNameEncry)
+        print("Client name", clientName, "is sent to the server.")
 
-                # Client sends name to server
-                clientName = input("Enter client name: ")
-                clientNameEncry = encryRSA(clientName, "server")
-                clientSocket.send(clientNameEncry)
-                print("Client name",clientName,"is sent to the server.")
+        # Client receives message from server.
+        # If the client name is not valid, the client with get the
+        # appropriate notice and connection will be terminated.
+        message = clientSocket.recv(2048).decode('ascii')
+        if (message == "Invalid clientName"):
+            print("Invalid client name.\nTerminating.")
+            clientSocket.close()
+            return
 
-                # Client receives message from server.
-                # If the client name is not valid, the client with get the
-                # appropriate notice and connection will be terminated.
-                message = clientSocket.recv(2048).decode('ascii')
-                if (message == "Invalid clientName"):
-                        print("Invalid client name.\nTerminating.")
-                        clientSocket.close()
-                        return
+        # Otherwise we continue with our program.
+        # The client receives the encrypted  sym_key form the server
+        # and decrypts it.
+        sym_key_Encry = clientSocket.recv(2048)
+        sym_key = decryRSA(sym_key_Encry, clientName)
+        print("Received the symmetric key from the server.")
 
-                # Otherwise we continue with our program.
-                # The client receives the encrypted  sym_key form the server
-                # and decrypts it.
-                sym_key_Encry = clientSocket.recv(2048)
-                sym_key = decryRSA(sym_key_Encry, clientName)
-                print("Received the symmetric key from the server.")
+        # Client receives AES encrypted message from server, asking for filename.
+        # Message is decrypted using sym_key.
+        messageEncry = clientSocket.recv(2048)
+        message = decryAES(messageEncry, sym_key)
+        print(message)
 
-                # Client receives AES encrypted message from server, asking for filename.
-                # Message is decrypted using sym_key.
-                messageEncry = clientSocket.recv(2048)
-                message = decryAES(messageEncry, sym_key)
-                print(message)
+        # Client inputs file name and sends to the server.
+        fileName = input("")
+        fileNameEncry = encryAES(fileName, sym_key)
+        clientSocket.send(fileNameEncry)
 
-                # Client inputs file name and sends to the server.
-                fileName = input("")
-                fileNameEncry = encryAES(fileName, sym_key)
-                clientSocket.send(fileNameEncry)
+        # recieve message from server
+        encryptedFileSizeReq = clientSocket.recv(2048)
+        fileSizeReq = decryAES(encryptedFileSizeReq, sym_key)
+        print(fileSizeReq)
 
-                # recieve message from server
-                encryptedFileSizeReq = clientSocket.recv(2048)
-                fileSizeReq = decryAES(encryptedFileSizeReq, sym_key)
-                print(fileSizeReq)
+        # Call getFile function to read in the file
+        data = getFile(fileName)
 
-                # Call getFile function to read in the file
-                fullData = getFile(fileName)
+        # Send the file size to the server
+        fileSize = len(data)
+        clientSocket.send(encryAES(str(fileSize), sym_key))
 
-                # Send the file size to the server
-                fileSize = len(fullData)
-                clientSocket.send(encryAES(str(fileSize), sym_key))
+        # Get message from server on whether file is too big or not
+        encryptedSizeMsg = clientSocket.recv(2048)
+        sizeMsg = decryAES(encryptedSizeMsg, sym_key)
+        print(sizeMsg)
 
-                # Get message from server on whether file is too big or not
-                encryptedSizeMsg = clientSocket.recv(2048)
-                sizeMsg = decryAES(encryptedSizeMsg, sym_key)
-                print(sizeMsg)
+        # Close the connection if the file size is too large
+        if (sizeMsg == "The file size is too large.\nTerminating"):
+            clientSocket.close()
+            return
 
-                # Close the connection if the file size is too large
-                if (sizeMsg == "The file size is too large.\nTerminating"):
-                        clientSocket.close()
-                        return
+        # print(fileName)
+        f = open(fileName, "rb")
 
-                f = open(fileName, "rb")
-                data = f.read(2048)
-                print(data)
-                # Send File to server
-                print("Sending the file contents to server.")
-                clientSocket.send(encryptFile(data, sym_key))
+        packet = (int(fileSize) // 2048) + 1
+        while (packet != 0):
+            data = f.read(2048)
+            print(data)
+            clientSocket.send(encryptFile(data, sym_key))
+            packet -= 1
+        f.close()
 
-                while (data):
-                        print(data)
-                        clientSocket.send(encryptFile(data, sym_key))
-                        data = f.read(2048)
+        # Send File to server
+        print("Sending the file contents to server.")
+        clientSocket.send(encryptFile(data, sym_key))
 
-                f.close()
-                # Get the confirmation message and print it
-                encryptedConfirmation = clientSocket.recv(2048)
-                confirmation = decryAES(encryptedConfirmation, sym_key)
-                print(confirmation)
+        # Get the confirmation message and print it
+        encryptedConfirmation = clientSocket.recv(2048)
+        confirmation = decryAES(encryptedConfirmation, sym_key)
+        print(confirmation)
 
-                # Client terminate connection with the server
-                clientSocket.close()
+        # Client terminate connection with the server
+        clientSocket.close()
 
-        except socket.error as e:
-                print('An error occured:', e)
-                clientSocket.close()
-                sys.exit(1)
+    except socket.error as e:
+        print('An error occured:', e)
+        clientSocket.close()
+        sys.exit(1)
 
-#---------------
+
+# ---------------
 client()
-
-
